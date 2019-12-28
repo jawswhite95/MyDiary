272a9622b2e550389bf0a5378faac027e991788d
jawswhite95 <jawswhite95@naver.com>
2019-12-28 12:57:20 +0900

    MyDiary commit

---
+++ screens/ListScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text} from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import CalendarPicker from 'react-native-calendar-picker';
+export default ListScreen = () => {
+    return (
+        <SafeAreaView>
+            <CalendarPicker/>
+        </SafeAreaView>
+        );
+}
---
+++ screens/ViewScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import CalendarPicker from 'react-native-calendar-picker';
+export default ViewScreen = () => {
+    return (
+        <SafeAreaView>
+            <CalendarPicker/>
+        </SafeAreaView>
+        );
+}
